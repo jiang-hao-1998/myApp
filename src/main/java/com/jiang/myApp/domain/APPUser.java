2bcf9c01ec120f13782dd03546bd040d287ae24d
jiang-hao-1998 <2191133831@qq.com>
2021-03-04 20:27:22 +0800

    jxgm本提交

---
+++ src/main/java/com/jiang/myApp/domain/APPUser.java
@@ -0,0 +1,10 @@
+package com.jiang.myApp.domain;
+
+import lombok.Data;
+
+@Data
+public class APPUser {
+    private String userName;
+    private String userPassword;
+    private Integer userAge;
+}
